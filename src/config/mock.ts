b5786605285c50f7dc069e6189125b4bf53f13ed
Jona <llamasjonat@gmail.com>
2025-04-30 16:08:00 -0300

    feat: added mock

---
+++ src/config/mock.ts
@@ -0,0 +1,13 @@
+export const EVENT = {
+  title: 'Bitcoin Pizza Day',
+  description: 'Conectá con la Comunidad Bitcoiner',
+  date: 'Viernes 23 de Enero - 19:00hs hasta las 02:00hs',
+  imageUrl: '',
+};
+
+export const TICKET = {
+  title: 'Entrada General',
+  value: 10,
+  currency: 'USD',
+  type: 'general',
+};
