735fe49df37b88bf96d509d4cb675b707dbad2ba
Jona <llamasjonat@gmail.com>
2025-05-02 19:08:34 -0300

    fix(config): changed mock data

--- src/config/mock.ts
+++ src/config/mock.ts
@@ -2,12 +2,12 @@ export const EVENT = {
   title: 'Bitcoin Pizza Day',
   description: 'Conecta con la Comunidad Bitcoiner',
   date: 'Viernes 23 de Mayo - 19:00hs hasta las 02:00hs',
-  imageUrl: 'background-pizza.png',
+  imageUrl: '',
 };
 
 export const TICKET = {
-  title: 'TARJETA METÁLICA',
-  value: 1,
+  title: 'Entrada General',
+  value: 15,
   currency: 'USD',
-  type: 'metal',
+  type: 'general',
 };
