6043b93dec318b9c50aff7ffe60c8ca4de8d6146
Agustin Kassis <webmaster@masize.com>
2025-05-05 18:29:13 -0300

    add validateEmail function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// validate email
+export function validateEmail(email: string) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
