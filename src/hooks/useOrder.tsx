69f4b562bc349bba1ac2cbe10b17922f86b80cc5
Jona <llamasjonat@gmail.com>
2025-05-06 01:14:06 -0300

    fix: general changes

--- src/hooks/useOrder.tsx
+++ src/hooks/useOrder.tsx
@@ -63,7 +63,7 @@ const useOrder = (): UseOrderReturn => {
   // Polling LUD-21.
   useEffect(() => {
     if (!invoice || isPaid) return;
-    alert('PERDONAMEEEE!');
+    // alert('PERDONAMEEEE!');
     const interval = setInterval(async () => {
       try {
         const res = await fetch('/api/ticket/verify', {
