b88c91d473150074a10d79afc042442e0441edcc
Agustin Kassis <webmaster@masize.com>
2025-05-06 17:58:58 -0300

    fix sendEmailOrder type

--- src/types/ses.d.ts
+++ src/types/ses.d.ts
@@ -2,7 +2,7 @@ export interface SESClientInterface {
   sendEmailOrder(
     email: string,
     orderId: string,
-    type: string,
+    type?: string,
     serial?: number | undefined
   );
   sendEmailNewsletter(email: string);
