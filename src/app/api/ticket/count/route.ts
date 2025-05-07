6871e0d437ccc78f37a2f0e3d8b785a12dc87ea3
Agustin Kassis <webmaster@masize.com>
2025-05-07 17:33:33 -0300

    force dismiss cache

--- src/app/api/ticket/count/route.ts
+++ src/app/api/ticket/count/route.ts
@@ -7,16 +7,18 @@ import * as Sentry from '@sentry/nextjs';
 import { TICKET } from '@/config/mock';
 
 export async function GET(req: NextRequest) {
+  console.info('Counting tickets...');
   try {
     if (req.method !== 'GET') {
       throw new AppError('Method not allowed', 405);
     }
 
     const totalTickets = await countTotalTickets(TICKET.type);
+    console.info(`Counted ${totalTickets} tickets`);
 
     const response = NextResponse.json({
       status: true,
-      data: { totalTickets },
+      data: { totalTickets, random: Math.random() },
     });
 
     // Prevent caching
